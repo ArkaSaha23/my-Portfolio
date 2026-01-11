2e6e148038eb60b5dcee8abe9d5189c5d093a3b8
Arka Saha <arkacomputer2004@gmail.com>
2026-01-11 16:02:03 +0530

    add changes 11

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -22,7 +22,7 @@ function About() {
         <p>
           Let's connect and create something amazing together.
         </p>
-          <a href="./Arka_Saha_Resume.pdf" download className="mt-4 inline-block bg-blue-500 text-white px-6 py-2 rounded-lg shadow-md hover:bg-blue-600 transition-colors duration-300 font-mono">
+          <a href="./https://docs.google.com/document/d/1yq7o-gUwwJ_B68jEFuejjmM69hqh3H0CNuVPVkhI_HY/edit?tab=t.0" download className="mt-4 inline-block bg-blue-500 text-white px-6 py-2 rounded-lg shadow-md hover:bg-blue-600 transition-colors duration-300 font-mono">
             Download Resume
           </a>  
           <a href="#contact" className="mt-4 ml-4 inline-block bg-green-500 text-white px-6 py-2 rounded-lg shadow-md hover:bg-green-600 transition-colors duration-300 font-mono">
